21f93b1af18025f43206609e6316f9625fa90cd2
Sulthon Abdul Malik <sulthon.abdmalik@gmail.com>
2022-12-17 19:16:57 +0700

    Add payments service

---
+++ payments-service/src/main/java/com/ssamsara98/estore/payments_service/PaymentserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ssamsara98.estore.payments_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentserviceApplication.class, args);
+	}
+
+}
